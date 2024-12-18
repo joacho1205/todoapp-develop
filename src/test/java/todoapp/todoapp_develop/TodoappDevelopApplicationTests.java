33959b526b824bcd77f53a8e47e2459993a5b7da
joacho <joacho1205@gmail.com>
2024-12-18 19:24:02 +0900

    프로젝트 생성

---
+++ src/test/java/todoapp/todoapp_develop/TodoappDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package todoapp.todoapp_develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
